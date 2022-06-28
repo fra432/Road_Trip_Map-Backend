e0ee9b073edf7a8f4c40f0a41361cd1c6bc41a0a
Francesco Fabrissin <francescofabrissin.gmail.com>
2022-06-28 11:45:51 +0200

    created database connection

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
 const debug = require("debug")("road_trip_map:server:index");
 const chalk = require("chalk");
+const connectDB = require("./database");
 const initializeServer = require("./server/initializeServer");
 
 const port = process.env.PORT ?? 4000;
+const connectionString = process.env.MONGO_CONNECTION;
 
 (async () => {
   try {
+    await connectDB(connectionString);
     await initializeServer(port);
   } catch {
     debug(chalk.red("Exiting with errors"));
