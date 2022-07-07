5b58ff8cff7054da4db9a77658d9991827a48d80
Francesco Fabrissin <francescofabrissin.gmail.com>
2022-07-07 12:55:33 +0200

    added password in user credentials

--- src/mocks/usersMock.js
+++ src/mocks/usersMock.js
@@ -1,10 +1,11 @@
 const mockUserLogin = {
-  username: "fra432",
+  email: "frafra@gmail.com",
   password: "$2a$10$fvX34xBeots6dRezw8IQJ.g/diQHdPQefkscaQdoBXqd5ThlSggAS",
 };
 
 const mockUserRegister = {
   username: "fra432",
+  email: "frafra@gmail.com",
   password: "fra432",
 };
 
